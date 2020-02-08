5d17ecd85194e09f1a50855e42ad9af18e0e75d5
unknown <tcutlip08@gmail.com>
2020-02-08 02:32:47 -0500

    highlighting and deleting highlights as you click around cells

--- client/src/components/NumberInput/NumberInput.js
+++ client/src/components/NumberInput/NumberInput.js
@@ -1,30 +1,32 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+// import { Link } from "react-router-dom";
+// import Container from "react-bootstrap/Container";
+// import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
 import "./NumberInput.css";
 
 const NumberInput = props => {
-  //   if (props.value) {
-  return (
-    <Col className={`cell c${props.c + 1} r${props.r + 1}`} xs={1}>
-      <div
-        // type="number"
-        name="cell"
+  if (props.value) {
+    return (
+      <span
+        className={`cell r${props.r + 1} c${props.c + 1} r${props.r +
+          1}c${props.c + 1}`}
         onClick={() => props.handleClick(props.r, props.c)}
       >
-        {props.value}&nbsp;
-      </div>
-    </Col>
-  );
-  //   } else {
-  //     return (
-  //       <Col className={`cell c${props.x + 1}`} xs={1}>
-  //         <input type="number" />
-  //       </Col>
-  //     );
-  //   }
+        {props.value}
+      </span>
+    );
+  } else {
+    return (
+      <span
+        className={`cell r${props.r + 1} c${props.c + 1} r${props.r +
+          1}c${props.c + 1}`}
+        onClick={() => props.handleClick(props.r, props.c)}
+      >
+        &nbsp;
+      </span>
+    );
+  }
 };
 
 export default NumberInput;
--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+// import Container from "react-bootstrap/Container";
+// import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
 import "./Landing.css";
 import Number from "../../components/NumberInput/NumberInput";
 
 class Landing extends Component {
+  // Minimum of 17 numbers in randomizer
   state = {
     number: "0123456789",
     clickedCell: "",
@@ -26,14 +27,13 @@ class Landing extends Component {
     document.addEventListener("keydown", this.onKeyDown);
   }
 
-  componentDidUpdate() {
-    console.log(this.state.clickedCell);
-  }
+  componentDidUpdate() {}
+
   componentWillUnmount() {
     document.removeEventListener("keydown", this.onKeyDown);
   }
+
   onKeyDown = e => {
-    // console.log(this.state.number.includes(e.key));
     if (this.state.number.includes(e.key)) {
       let grid = this.state.grid;
       let cell = this.state.clickedCell;
@@ -43,20 +43,119 @@ class Landing extends Component {
   };
 
   handleClick(r, c) {
+    if (this.state.clickedCell) {
+      this.clearClickedCellStyling(
+        parseInt(this.state.clickedCell[0]),
+        parseInt(this.state.clickedCell[1])
+      );
+    }
     this.setState({ clickedCell: `${r}${c}` });
-    console.log(r);
-    console.log(c);
+
+    this.highlightCells(r, c);
+  }
+
+  highlightCells(r, c) {
+    // Row Clicked
+    let rowArray = document.getElementsByClassName(`r${r + 1}`);
+    for (let i = 0; i < rowArray.length; i++) {
+      rowArray[i].style.backgroundColor = "grey";
+    }
+
+    // Col Clicked
+    let colArray = document.getElementsByClassName(`c${c + 1}`);
+    for (let i = 0; i < colArray.length; i++) {
+      colArray[i].style.backgroundColor = "grey";
+    }
+
+    if (r < 3 && c < 3) {
+      this.loopThroughHighlightCells(3, 3, true);
+    } else if (r < 3 && c < 6) {
+      this.loopThroughHighlightCells(3, 6, true);
+    } else if (r < 3 && c < 9) {
+      this.loopThroughHighlightCells(3, 9, true);
+    } else if (r < 6 && c < 3) {
+      this.loopThroughHighlightCells(6, 3, true);
+    } else if (r < 6 && c < 6) {
+      this.loopThroughHighlightCells(6, 6, true);
+    } else if (r < 6 && c < 9) {
+      this.loopThroughHighlightCells(6, 9, true);
+    } else if (r < 9 && c < 3) {
+      this.loopThroughHighlightCells(9, 3, true);
+    } else if (r < 9 && c < 6) {
+      this.loopThroughHighlightCells(9, 6, true);
+    } else if (r < 9 && c < 9) {
+      this.loopThroughHighlightCells(9, 9, true);
+    }
+
+    // Cell Clicked
+    document.getElementsByClassName(
+      `r${r + 1}c${c + 1}`
+    )[0].style.backgroundColor = "lightblue";
+  }
+
+  loopThroughHighlightCells(r, c, bool) {
+    if (bool) {
+      for (let row = r - 3; row < r; row++) {
+        for (let col = c - 3; col < c; col++) {
+          document.getElementsByClassName(
+            `r${row + 1}c${col + 1}`
+          )[0].style.backgroundColor = "grey";
+        }
+      }
+    } else if (!bool) {
+      for (let row = r - 3; row < r; row++) {
+        for (let col = c - 3; col < c; col++) {
+          document.getElementsByClassName(
+            `r${row + 1}c${col + 1}`
+          )[0].style.backgroundColor = "";
+        }
+      }
+    }
+  }
+
+  clearClickedCellStyling(r, c) {
+    document.getElementsByClassName(
+      `r${r + 1}c${c + 1}`
+    )[0].style.backgroundColor = "";
+
+    let rowArray = document.getElementsByClassName(`r${r + 1}`);
+    for (let i = 0; i < rowArray.length; i++) {
+      rowArray[i].style.backgroundColor = "";
+    }
+
+    let colArray = document.getElementsByClassName(`c${c + 1}`);
+    for (let i = 0; i < colArray.length; i++) {
+      colArray[i].style.backgroundColor = "";
+    }
+
+    if (r < 3 && c < 3) {
+      this.loopThroughHighlightCells(3, 3, false);
+    } else if (r < 3 && c < 6) {
+      this.loopThroughHighlightCells(3, 6, false);
+    } else if (r < 3 && c < 9) {
+      this.loopThroughHighlightCells(3, 9, false);
+    } else if (r < 6 && c < 3) {
+      this.loopThroughHighlightCells(6, 3, false);
+    } else if (r < 6 && c < 6) {
+      this.loopThroughHighlightCells(6, 6, false);
+    } else if (r < 6 && c < 9) {
+      this.loopThroughHighlightCells(6, 9, false);
+    } else if (r < 9 && c < 3) {
+      this.loopThroughHighlightCells(9, 3, false);
+    } else if (r < 9 && c < 6) {
+      this.loopThroughHighlightCells(9, 6, false);
+    } else if (r < 9 && c < 9) {
+      this.loopThroughHighlightCells(9, 9, false);
+    }
   }
 
   render() {
     return (
-      <Container className="Landing">
+      <div className="Landing">
         {this.state.grid.map((row, r) => {
-          // console.log(row);
           return (
-            <Row className={`r${r + 1} justify-content-md-center`}>
+            <div className={`row`}>
               {row.map((col, c) => {
-                // console.log(col);
                 return (
                   <Number
                     c={c}
@@ -66,10 +165,10 @@ class Landing extends Component {
                   />
                 );
               })}
-            </Row>
+            </div>
           );
         })}
-      </Container>
+      </div>
     );
   }
 }
